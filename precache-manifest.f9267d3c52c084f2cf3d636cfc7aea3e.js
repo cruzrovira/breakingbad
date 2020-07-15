f59fda29b39065848d80ba692492153cb039b15b
Oscar Alfredo  Cruz Rovira <cruzrovira@gmail.com>
2020-07-14 22:46:47 -0600

    Updates

---
+++ precache-manifest.f9267d3c52c084f2cf3d636cfc7aea3e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "340775c60afad132fbf6d185d0cc3c4b",
+    "url": "/breakingbad/index.html"
+  },
+  {
+    "revision": "daeb88571cc18590d8fe",
+    "url": "/breakingbad/static/css/main.bf33d6e2.chunk.css"
+  },
+  {
+    "revision": "527a24d93b2ca82bb888",
+    "url": "/breakingbad/static/js/2.0bf547c5.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/breakingbad/static/js/2.0bf547c5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "daeb88571cc18590d8fe",
+    "url": "/breakingbad/static/js/main.372b5101.chunk.js"
+  },
+  {
+    "revision": "ea2d3b5d3b339cb26b3a",
+    "url": "/breakingbad/static/js/runtime-main.e09d8a2d.js"
+  },
+  {
+    "revision": "a8bde326e2357465b62604589bae2b16",
+    "url": "/breakingbad/static/media/logo.a8bde326.png"
+  }
+]);
